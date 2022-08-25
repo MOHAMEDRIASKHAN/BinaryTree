3146a43559575930e726b9c093d489f1c362624a
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-25 20:29:39 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace BinarySearchTree
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+           
+
+            int[] arr = { 70, 30, 42, 55, 10 };
+            int temp;
+            for(int j=0;j<=arr.Length-2;j++)
+            {
+                for(int i=0;i<=arr.Length-2;i++)
+                {
+                    if (arr[i] > arr[i+1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i+1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted Array");
+            foreach (int ele in arr)
+            {
+                Console.Write(ele + " ");
+            }
+        }
+    }
+}
